93babe63e9ff09ea3aea66dd464dd4ae1581ac63
Michał Kalbarczyk <fazibear@gmail.com>
2016-03-18 11:27:55 +0100

    fix mix.exs

--- mix.exs
+++ mix.exs
@@ -10,6 +10,14 @@ defmodule ExIcal.Mixfile do
      deps: deps]
   end
 
+  def package do
+    [
+      maintainers: ["Michał Kalbarczyk"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/fazibear/ex_ical"}
+   ]
+  end
+
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
@@ -19,7 +27,8 @@ defmodule ExIcal.Mixfile do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.0"},
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev},
       {:timex, "~> 1.0"}
     ]
   end
