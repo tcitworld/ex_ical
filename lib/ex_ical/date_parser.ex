ff559332b6cb6a2e6badd44e765b749545ab6c7b
Michał Kalbarczyk <fazibear@gmail.com>
2016-04-11 18:34:42 +0200

    fix timezone

--- lib/ex_ical/date_parser.ex
+++ lib/ex_ical/date_parser.ex
@@ -6,20 +6,20 @@ defmodule ExIcal.DateParser do
   def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2), "T",
                         hour :: binary-size(2), minutes :: binary-size(2), seconds :: binary-size(2), "Z" >>, nil) do
       %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer,
-                hour: hour |> String.to_integer, minute: minutes |> String.to_integer, second: seconds |> String.to_integer, timezone: %TimezoneInfo{abbreviation: "UTC"}}
+                hour: hour |> String.to_integer, minute: minutes |> String.to_integer, second: seconds |> String.to_integer, timezone: Timezone.get(:utc)}
   end
 
   def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2), "Z" >>, nil) do
-      %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer, timezone: %TimezoneInfo{abbreviation: "UTC"}}
+      %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer, timezone: Timezone.get(:utc)}
   end
 
   def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2), "T",
                         hour :: binary-size(2), minutes :: binary-size(2), seconds :: binary-size(2) >>, timezone) do
       %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer,
-                hour: hour |> String.to_integer, minute: minutes |> String.to_integer, second: seconds |> String.to_integer, timezone: %TimezoneInfo{abbreviation: timezone}}
+                hour: hour |> String.to_integer, minute: minutes |> String.to_integer, second: seconds |> String.to_integer, timezone: Timezone.get(timezone)}
   end
 
   def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2) >>, timezone) do
-      %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer, timezone: %TimezoneInfo{abbreviation: timezone}}
+      %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer, timezone: Timezone.get(timezone)}
   end
 end
