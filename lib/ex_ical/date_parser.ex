5d45bcc6225ff07ce48219d4b5bb294602a8ea86
Michał Kalbarczyk <fazibear@gmail.com>
2016-04-11 16:07:44 +0200

    support global timezone

--- lib/ex_ical/date_parser.ex
+++ lib/ex_ical/date_parser.ex
@@ -1,13 +1,15 @@
 defmodule ExIcal.DateParser do
   use Timex
 
+  def parse(data), do: parse(data, nil)
+
   def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2), "T",
-                        hour :: binary-size(2), minutes :: binary-size(2), seconds :: binary-size(2), "Z" >>) do
+                        hour :: binary-size(2), minutes :: binary-size(2), seconds :: binary-size(2), "Z" >>, nil) do
       %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer,
                 hour: hour |> String.to_integer, minute: minutes |> String.to_integer, second: seconds |> String.to_integer, timezone: %TimezoneInfo{abbreviation: "UTC"}}
   end
 
-  def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2), "Z" >>) do
+  def parse(<< year :: binary-size(4), month :: binary-size(2), day :: binary-size(2), "Z" >>, nil) do
       %DateTime{year: year |> String.to_integer, month: month |> String.to_integer, day: day |> String.to_integer, timezone: %TimezoneInfo{abbreviation: "UTC"}}
   end
 
--- lib/ex_ical/parser.ex
+++ lib/ex_ical/parser.ex
@@ -3,41 +3,43 @@ defmodule ExIcal.Parser do
   alias ExIcal.Event
 
   def parse(data) do
-    data |> String.split("\n") |> Enum.reduce([], fn(line, events) ->
-      parse_line(String.strip(line), events)
-    end)
+    data |> String.split("\n") |> Enum.reduce(%{events: []}, fn(line, data) ->
+      parse_line(String.strip(line), data)
+    end) |> Map.get(:events)
   end
 
-  defp parse_line("BEGIN:VEVENT" <> _, events),          do: [%Event{}] ++ events
-  defp parse_line("DTSTART" <> start, events)            when length(events) > 0, do: events |> put_to_map(:start, process_date(start))
-  defp parse_line("DTEND" <> endd, events)               when length(events) > 0, do: events |> put_to_map(:end, process_date(endd))
-  defp parse_line("DTSTAMP" <> stamp, events)            when length(events) > 0, do: events |> put_to_map(:stamp, process_date(stamp))
-  defp parse_line("SUMMARY:" <> summary, events)         when length(events) > 0, do: events |> put_to_map(:summary, summary)
-  defp parse_line("DESCRIPTION:" <> description, events) when length(events) > 0, do: events |> put_to_map(:description, description)
-  defp parse_line("RRULE:" <> rrule, events)             when length(events) > 0, do: events |> put_to_map(:rrule, process_rrule(rrule))
-  defp parse_line(_, events), do: events
+  defp parse_line("BEGIN:VEVENT" <> _, data),           do: %{events: [%Event{}] ++ data[:events]}
+  defp parse_line("DTSTART" <> start, data),            do: data |> put_to_map(:start, process_date(start, data[:tzid]))
+  defp parse_line("DTEND" <> endd, data),               do: data |> put_to_map(:end, process_date(endd, data[:tzid]))
+  defp parse_line("DTSTAMP" <> stamp, data),            do: data |> put_to_map(:stamp, process_date(stamp, data[:tzid]))
+  defp parse_line("SUMMARY:" <> summary, data),         do: data |> put_to_map(:summary, summary)
+  defp parse_line("DESCRIPTION:" <> description, data), do: data |> put_to_map(:description, description)
+  defp parse_line("RRULE:" <> rrule, data),             do: data |> put_to_map(:rrule, process_rrule(rrule, data[:tzid]))
+  defp parse_line("TZID:" <> tzid, data),               do: data |> Map.put(:tzid, tzid)
+  defp parse_line(_, data), do: data
 
-  defp put_to_map(events, key, value) do
+  defp put_to_map(%{events: events} = data, key, value) do
     [ event | other ] = events
     event = %{ event | key => value}
-    [event] ++ other
+    %{ data | events: [event] ++ other }
   end
 
-  defp process_date(":" <> date), do: DateParser.parse(date)
-  defp process_date(";" <> date) do
+  defp process_date(":" <> date, tzid), do: DateParser.parse(date, tzid)
+  defp process_date(":" <> date, tzid), do: DateParser.parse(date, tzid)
+  defp process_date(";" <> date, _) do
     [timezone, date] = date |> String.split(":")
     timezone = case timezone do
       "TZID=" <> timezone -> timezone
-      _ -> "UTC"
+      _ -> nil
     end
     DateParser.parse(date, timezone)
   end
 
-  defp process_rrule(rrule) do
+  defp process_rrule(rrule, tzid) do
     rrule |> String.split(";") |> Enum.reduce(%{}, fn(rule, hash) ->
       [key, value] = rule |> String.split("=")
       case key |> String.downcase |> String.to_atom do
-        :until -> hash |> Map.put(:until, DateParser.parse(value))
+        :until -> hash |> Map.put(:until, DateParser.parse(value, tzid))
         :interval -> hash |> Map.put(:interval, String.to_integer(value))
         :count -> hash |> Map.put(:count, String.to_integer(value))
         :freq -> hash |> Map.put(:freq, value)
