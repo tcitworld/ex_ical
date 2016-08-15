67c0eed9bed4a5772456757f8ed075b16ac5d440
Lee Sharma <lee@leesharma.com>
2016-08-15 18:28:20 -0400

    add tests for all valid date input formats
    The big change in this commit is the new
    `test/ex_ical_date_format_test.ex` test. It tests all 6 valid date
    formats against both a TZID-free calendar and a calendar with the global
    TZID set.
    
    The testcases are declared in a comprehension because this makes
    adding/removing/managing input formats and desired outputs trivial
    (versus managing a dozen testcases).

--- test/ex_ical/date_parser_test.exs
+++ test/ex_ical/date_parser_test.exs
@@ -1,22 +1,3 @@
-defmodule ExIcal.Test.Utils do
-  def subset?(nil, nil), do: true
-  def subset?(nil, _), do: false
-  def subset?(_, nil), do: false
-  def subset?(%{} = subset, %{} = superset) do
-    superset_contains_pair = fn {k, v} ->
-      %{superset | k => v} === superset
-    end
-    subset |> Enum.all?(superset_contains_pair)
-  end
-
-  defmacro assert_subset(subset, superset) do
-    quote do
-      assert subset?(unquote(subset), unquote(superset)),
-        "Expected #{inspect unquote(subset)} to be a subset of #{inspect unquote(superset)}"
-    end
-  end
-end
-
 defmodule ExIcal.DateParserTest do
   use ExUnit.Case
   import ExIcal.Test.Utils
