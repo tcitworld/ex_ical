83c0e46a97805bbae024efc86d671f581c12d6b1
Michał Kalbarczyk <fazibear@gmail.com>
2016-11-02 13:56:17 +0100

    Update deps

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule ExIcal.Mixfile do
     [
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, ">= 0.0.0", only: :dev},
-      {:credo, "~> 0.4.0", only: [:dev, :test]},
+      {:credo, "~> 0.4.0", only: :dev},
       {:timex, "~> 1.0"}
     ]
   end
